dab7344e25f49e44ba4c4a27357b58fe92a8409b
22041097-Chen Chu Jie <1003362127@qq.com>
2022-09-20 11:11:31 +0800

    sol later

--- train.py
+++ train.py
@@ -5,9 +5,17 @@ def TrainIter():
     board = CreateGame(height, width)
     Play(board, [None, AIPlayer, AIPlayer], display=True)
 
-Load(0)
-cnt = 0
-while True:
-    print("Iter", cnt)
-    TrainIter()
-    Dump(cnt % 4)
+
+def Run():
+    print("Loading previous results...")
+    Load(0)
+    print("Begin training.")
+    cnt = 0
+    while True:
+        print("Iter", cnt)
+        TrainIter()
+        Dump(cnt % 4)
+        cnt += 1
+
+if __name__ == '__main__':
+    Run()
