dcf744921a79141b1589ec2c75b2327b5244d542
cjj <1003362127@qq.com>
2022-09-24 21:44:24 +0800

    ai after debug

--- rules.py
+++ rules.py
@@ -1,6 +1,6 @@
 from tarfile import RECORDSIZE
 import numpy as np  #使用numpy库里的随机函数
-from .ai import *
+#from .history import *
 BLACK = 1  #棋盘格子属性 黑棋 白棋 无棋
 WHITE = 2
 EMPTY = 0
@@ -18,6 +18,20 @@ directions = [      #表示格子的四个方向
 def CreateGame(h, w):
     return np.zeros((h, w)).astype(np.uint8)  #使用库函数 生产一个15*15的矩阵 然后将其数据类型转化成无符号8位（最小的类型）
 
+def CurStep(board): #非空格子数量
+    res = 0
+    for i in range(board.shape[0]):
+        for j in range(board.shape[1]):
+            res += 1 if board[i,j] != 0 else 0
+    return res
+def Serialize(board):  #把棋盘转化成一个数字  用于哈希
+    res = ''
+    for i in range(board.shape[0]):
+        tmp = 0
+        for j in range(board.shape[1]):
+            tmp = tmp * 3 + board[i,j]
+        res += str(tmp)
+    return res
 def PositionInBoard(board, x, y):    #判断落子是否在棋盘内
     h, w = board.shape  
     return (0<=x) and (x<h) and (0<=y) and (y<w)
@@ -29,8 +43,11 @@ def PositionInBoard(board, x, y):    #判断落子是否在棋盘内
 def Step(board, player, stepFunc):    #棋来！
     x, y = stepFunc(board, player)
 
+    if(CurStep(board)==225): #和棋则白棋赢
+        return 2,x,y
     # 判断是否合法
     if ((not PositionInBoard(board, x, y)) or board[x,y]!=EMPTY): #如果落子不合法 直接判对手赢
+        printf("落子不合法")
         return 3 - player, x, y
     board[x, y] = player
 
@@ -91,11 +108,9 @@ def Play(board, stepFuncs, display = True):
             break
         else:
             currentPlayer = 3 - currentPlayer  #切换对手
-    
     for i in history:
         record[i][0]+=1
         record[i][res]+=1
-
     return res
 
 def Player(board, player): 
